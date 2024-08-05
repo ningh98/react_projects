808c45174e39669ce8b10c4502b90d7617cceab8
Ninghui Cai <ninghuicai@Ninghuis-MacBook-Pro.local>
2024-08-05 07:37:23 -0700

    image-slider

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import './App.css';
-import Accordion from './components/accordion';
-import RandomColor from './components/random-color';
-import StarRating from './components/star-rating';
+import "./App.css";
+import Accordion from "./components/accordion";
+import ImageSlider from "./components/image-slider";
+import RandomColor from "./components/random-color";
+import StarRating from "./components/star-rating";
 
 function App() {
   return (
@@ -11,7 +12,13 @@ function App() {
       {/* Random Color component */}
       {/* <RandomColor/> */}
       {/* Star Rating component */}
-      <StarRating noOfStars={10}/>
+      {/* <StarRating noOfStars={10}/> */}
+      {/* Image slider Component */}
+      <ImageSlider
+        url={"https://picsum.photos/v2/list"}
+        page="1"
+        limit={"10"}
+      />
     </div>
   );
 }
